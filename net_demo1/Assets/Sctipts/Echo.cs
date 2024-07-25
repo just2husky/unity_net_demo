9accc34c1e001f44b40914a92708d1ec2f4dc589
张超，Zhang Chao <zhangchao_shandong@163.com>
2024-07-26 00:58:22 +0800

    init

---
+++ net_demo1/Assets/Sctipts/Echo.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using UnityEngine.UI;
+
+using TMPro;
+
+public class Echo : MonoBehaviour
+{
+    //定义套接字
+    Socket socket;
+    //UGUI
+    public TMP_InputField InputFeld;
+    public TMP_Text text;
+
+    //点击连接按钮
+    public void Connection()
+    {
+        //Socket
+        socket = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream, ProtocolType.Tcp);
+        //Connect
+        socket.Connect("127.0.0.1", 8888);
+    }
+
+    //点击发送按钮
+    public void Send()
+    {
+        //Send
+        string sendStr = InputFeld.text;
+        byte[] sendBytes = System.Text.Encoding.Default.GetBytes(sendStr);
+        socket.Send(sendBytes);
+        //Recv
+        byte[] readBuff = new byte[1024];
+        int count = socket.Receive(readBuff);
+        string recvStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+        text.text = recvStr;
+        //Close
+        socket.Close();
+    }
+}
---
+++ net_demo1_server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+namespace EchoServer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! ");
+            //Socket
+            Socket listenfd = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            //Bind
+            IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEp = new IPEndPoint(ipAdr, 8888);
+            listenfd.Bind(ipEp);
+            //Listen
+            listenfd.Listen(0);
+            Console.WriteLine("[服务器]启动成功");
+            while (true)
+            {
+                //Accept
+                Socket connfd = listenfd.Accept();
+                Console.WriteLine("[服务器]Accept");
+                //Receive
+                byte[] readBuff = new byte[1024];
+                int count = connfd.Receive(readBuff);
+                string readStr = System.Text.Encoding.Default.GetString(readBuff,
+                                  0, count);
+                Console.WriteLine("[服务器接收]" + readStr);
+                //Send
+                byte[] sendBytes = System.Text.Encoding.Default.GetBytes(readStr);
+                connfd.Send(sendBytes);
+            }
+        }
+    }
+}
